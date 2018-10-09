a259f96d2042fd1e3d99de65611d7bb046e5bba3
aarnej <aarnej@iki.fi>
2018-10-09 20:09:13 +0300

    showDebugTab enabled by default

--- rpcs3/rpcs3qt/gui_settings.h
+++ rpcs3/rpcs3qt/gui_settings.h
@@ -193,7 +193,7 @@ namespace gui
 	const gui_save m_currentConfig     = gui_save(meta, "currentConfig",     Settings);
 	const gui_save m_currentStylesheet = gui_save(meta, "currentStylesheet", Default);
 	const gui_save m_saveNotes         = gui_save(meta, "saveNotes",         QVariantMap());
-	const gui_save m_showDebugTab      = gui_save(meta, "showDebugTab",      false);
+	const gui_save m_showDebugTab      = gui_save(meta, "showDebugTab",      true);
 	const gui_save m_enableUIColors    = gui_save(meta, "enableUIColors",    false);
 	const gui_save m_richPresence      = gui_save(meta, "useRichPresence",   true);
 	const gui_save m_discordState      = gui_save(meta, "discordState",      "");
